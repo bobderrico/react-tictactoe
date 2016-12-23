522ea03aeda669b7eccb95e7e48dd637786e6fc8
Bob D'Errico <bob.derrico@doradosystems.com>
2016-12-23 11:12:18 -0500

    Cleans up logic for creating rows and squares

--- board.jsx
+++ board.jsx
@@ -4,29 +4,25 @@ import {List} from 'immutable';
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.props.squares.get(i)} onClick={() => this.props.onClick(i)}/>;
+    return <Square key={i} value={this.props.squares.get(i)} onClick={() => this.props.onClick(i)}/>;
+  }
+
+  renderRow(rowNumber) {
+    var squares = [];
+    for (let i = 0; i < 3; i++) {
+      squares.push(this.renderSquare(i + rowNumber));
+    }
+
+    return <div key={rowNumber} className="board-row">{squares}</div>
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    var rows = [];
+    for (let i = 0; i < 9; i += 3) {
+      rows.push(this.renderRow(i));
+    }
+
+    return <div>{rows}</div>;
   }
 }
 
